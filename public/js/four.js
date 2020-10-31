745d38b0c1ef9627b282df0965ebdf37bf28bb7e
jinnyjung <jiiiiin.com@gmail.com>
2020-10-31 12:36:39 +0900

    git commit staged

---
+++ public/js/four.js
@@ -0,0 +1,22 @@
+var t = function( p ) { 
+	var x = 100.0; 
+	var y = 100; 
+	var speed = 2.5; 
+	p.setup = function() {
+	  p.createCanvas(300, 200);
+	};
+  
+	p.draw = function() {
+	  p.background(100);
+	  p.fill(1);
+	  x += speed; 
+	  if(x > p.width){
+		x = 0; 
+	  }
+	  p.ellipse(x,y,50,50);
+  
+	};
+  };
+  document.addEventListener("DOMContentLoaded", function(){
+    var myp5 = new p5(t, 'c2');
+});
