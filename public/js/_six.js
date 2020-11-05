7c55ca26e4c7e8b0df67dd4066dd9a08a475bb37
jinnyjung <jiiiiin.com@gmail.com>
2020-11-05 20:52:54 +0900

    git commit message

---
+++ public/js/_six.js
@@ -0,0 +1,34 @@
+let sixSketch = function(p) {
+
+  
+    p.setup = function() {
+      p.createCanvas(1000,500);
+      p.noStroke();
+      p.colorMode(p.HSB);
+    }
+  
+    p.draw = function() {
+      tick+=1;
+      p.clear();
+
+      p.fill(255,0.5);
+    //   p.stroke(1);
+      p.rect(0, 0, p.width/2, p.height);
+
+      for (var i=0; i<25; i++){
+        draw_text(i, (p.random(1000)*0.24));
+      }
+  
+    }
+    let draw_text = function(i, n){
+      p.fill(100);
+      p.text(n + '   now on working' + '    ' + n * 0.00001 + '   we got this', p.width/2+10, 17 * (i+1));
+    }
+
+  }
+  
+  document.addEventListener("DOMContentLoaded", function(){
+	var myp5 = new p5(sixSketch, 'c6');
+	});
+
+  
