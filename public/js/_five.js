b547ec9b8a7121e2107570063554f691e0a40abe
jinnyjung <jiiiiin.com@gmail.com>
2020-12-21 17:08:10 +0900

    git commit message

--- public/js/_five.js
+++ public/js/_five.js
@@ -1,120 +1,35 @@
 let fiveSketch = function (p) {
-  let THE_SEED;
-
-  let width =700;
-
-  let resolution =20;
-
-  let noise_zoom = 15;
-  let magnitude = 100;
-
-  let plate_padding =15; // make space
-
-  let number_of_blocks = 10;
-  let blocks = [];
-
-  let palette;
-
-  p.setup = function () {
-    p.createCanvas(1000, 500);
-    p.clear();
-    p.noLoop();
-    p.strokeWeight(0.1);
-
-    palette = [
-      p.color(86, 86, 86, 100),
-      p.color(196, 196, 199, 100),
-      p.color(33, 46, 66, 100),
-      p.color(94, 94, 94, 100),
-      p.color(42, 60, 110, 100),
-      p.color(227, 227, 227, 100)
-    ];
-
-    THE_SEED = p.floor(p.random(9999999));
-    p.noiseSeed(THE_SEED);
-
-    for (var i = 0; i < number_of_blocks; i++) {
-      blocks.push(create_block(p.random(1, 10), i));
-    }
-  };
-
-  p.draw = function () {
-    p.background(235, 235, 235);
-    let current_height = 0;
-    
-    p.fill(100);
+  let rectX = 568;
+	let rectY = 32;
+	let rectW = 380;
+	let rectH = 180;
+  let spaceD = 17;
   
     
-    p.translate(-300, p.height);
-
-    blocks.forEach(function (block, index) {
-      let block_height = p.abs(Math.min(...block[block.length - 1].points) - 10);
-
-      if (current_height + block_height < p.height -100) {
-        display_block(block, index);
-        p.translate(0, -block_height);
-        current_height += block_height;
-      } else {
-        p.translate(190, current_height);
-        display_block(block, index);
-        p.translate(0, -block_height);
-        current_height = block_height;
-      }
-    }, this);
-  };
-
-  function create_block(number_of_plates, block_index) {
-    let plates = [];
-    for (var plate_index = 0; plate_index < number_of_plates; plate_index++) {
-      let points = [];
-
-      let plate_height = p.randomGaussian(0,110);
-      if (plate_index > 0) {
-        for (let i = 0; i <= resolution; i++) {
-          points.push(
-            p.min(-plate_padding, get_noise(i, plate_index, block_index) - plate_height) +
-            plates[plate_index - 1].points[i]
-          );
-        }
-      } else {
-        for (let i = 0; i <= resolution; i++) {
-          points.push(p.min(-plate_padding, get_noise(i, plate_index, block_index) - plate_height));
-        }
-      }
-
-      plates.push({
-        points: points,
-        color: palette[p.floor(p.random(palette.length))]
-      });
-    }
-    return plates;
+	p.setup = function () {
+	  p.createCanvas(1000, 500);
+	  p.frameRate(60);
+    p.noLoop();
+	 
+	};
+  
+	p.draw = function () {
+	  // p.background(100);
+	  p.clear();
+  
+	  p.fill(250, 255);
+	  p.noStroke();
+	  p.rect(rectX, rectY, rectW, rectH);
+  
+	  p.textSize(12);
+	
+	  p.fill(0);
+    p.text('전시가 종료된 후', rectX+90 , rectY + 50 + (spaceD * 1));
+    p.text('«Data Composition -1»', rectX+90 , rectY + 50 + (spaceD * 2));
+    p.text('dc.seoul.kr 사이트에서', rectX+90 , rectY + 50 + (spaceD * 3));
+    p.text('모인 데이터를 활용한 사운드 작품이 발표됩니다.', rectX+90 , rectY + 50 + (spaceD * 4));
   }
-
-  function display_block(block, block_index) {
-    block.forEach(function (plate, index) {
-      p.fill(plate.color);
-      p.beginShape();
-
-      if (index === 0) {
-        p.vertex(0, 0);
-        p.vertex(100 + (p.random(width)), 0);
-      } else {
-        for (let i = 0; i <= resolution; i++) {
-          p.vertex(i * p.random(width) / resolution, block[index - 1].points[i] - plate_padding);
-        }
-      }
-      for (let i = resolution; i >= 0; i--) {
-        p.vertex(i * p.random(width) / resolution, block[index].points[i]);
-      }
-      p.endShape(p.CLOSE);
-
-    }, this);
-  }
-
-  function get_noise(x, y, z) {
-    return -magnitude * (p.noise(x / noise_zoom, y, z) - 0.4);
-  }
-};
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   var myp5 = new p5(fiveSketch, 'c5');
--- public/js/_one.js
+++ public/js/_one.js
@@ -1,20 +1,22 @@
 let oneSketch = function (p) {
-	let rectX = 185;
-	let rectY = 117;
-	let rectW = 600;
-	let rectH = 350;
+	
+	let rectX = 19;
+	let rectY = 280;
+	let rectW = 493;
+	let rectH = 182;
 	let spaceD = 17;
-  
+	 
+
 	let visitH;
 	let visitM;
 	let visitS;
   
-  
-  
 	p.setup = function () {
 	  p.createCanvas(1000, 500);
 	  p.frameRate(37);
-  
+      p.noLoop();
+
+
 	  visitH = p.hour();
 	  visitM = p.minute();
 	  visitS = p.second();
@@ -30,18 +32,11 @@ let oneSketch = function (p) {
   
 	  p.textSize(12);
 	  p.fill(117);
-	  p.text('전시 <Data Composition> 은 2021년 1월 15일부터 시작합니다.', rectX + 10, rectY + 20);
+	  p.text('전시 «Data Composition» 은 2021년 1월 15일부터 시작합니다.', rectX + 10, rectY + 20);
 	  p.fill(0);
-	  p.text('dc.seoul.kr 웹 사이트에서는 방문객이', rectX + 180, rectY + 100);
-	  p.text('웹 페이지에 방문한 시각과 머무른 시간을 수집합니다.', rectX + 180, rectY + 100 + spaceD);
-	  p.text('수집된 데이터는 작품 <Data Composition>에 사용됩니다.', rectX + 180, rectY + 100 + (spaceD * 2));
-	  p.text('접속한 시간 : ' + visitH + ' 시 ' + visitM + ' 분 ' + visitS + ' 초 ', rectX + 180, rectY + 100 + (spaceD * 4));
-	  p.text('지금의 시간 : ' + p.hour() + ' 시 ' + p.minute() + ' 분 ' + p.second() + ' 초 ', rectX + 180, rectY + 100 + (spaceD * 5));
-	  p.text('머물고 있는 순간 (Δ1/37) : Δ ' + p.int(p.frameCount), rectX + 180, rectY + 100 + (spaceD * 7));
-  
-	  p.text('© Data Composition 2020 by GRAYCODE,jiiiin', rectX + 180, rectY + 100 + (spaceD * 13));
-
-  
+	  p.text('dc.seoul.kr 웹 사이트에서는 방문객이', rectX + 100, rectY + 80);
+	  p.text('웹 페이지에 방문한 시각과 머무른 시간을 수집합니다.', rectX + 100, rectY + 80 + spaceD);
+	  p.text('수집된 데이터는 작품 «Data Composition»에 사용됩니다.', rectX + 100, rectY + 80 + (spaceD * 2));
 	};
 };
 
